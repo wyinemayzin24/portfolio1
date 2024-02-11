1d8ae5643d4ade3c6477c379d6437dcf8df02122
wgithub24 <wgithub24@gmail.com>
2024-02-11 23:50:08 +0630

    1st

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+ScrollReveal().reveal('.headline',{
+  delay : 300,
+  origin:'buttom',
+  distance: '100px',
+  interval:100,
+  opacity: 0.5,
+  scale:0.2,
+  reset:true
+
+});
+
+ScrollReveal().reveal('.aboutme',{
+  delay : 400,
+  origin:'left',
+  rotate: {  y: 9, z: 9 },
+
+  distance: '100px',
+  interval:100,
+  scale:0.5,
+  reset:true
+
+});
+ScrollReveal().reveal('.skill',{
+  delay : 500,
+  origin:'top',
+  distance: '100px',
+  rotate: {y: 9, z: 9 },
+  interval:100,
+  scale:0.1,
+  easing   : 'ease-in-out',
+  reset:true
+
+});
+
+ScrollReveal().reveal('.contact',{
+  delay : 500,
+  origin:'right',
+  distance: '100px',
+  rotate: {y: 9, z: 9 },
+  interval:100,
+  scale:0.1,
+  easing   : 'linear',
+  reset:true
+
+});
